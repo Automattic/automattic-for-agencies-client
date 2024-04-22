4b0c94294f87da15f40ba50fadeb7a3904a40bd1
nateweller <nateweller@users.noreply.github.com>
2024-04-22 19:58:24 +0000

    A4A: Add "wp-admin-url" query parameter to dashboard link. (#36994)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8789928490
    
    Upstream-Ref: Automattic/jetpack@73b9847b56e3c7346f88be0bbab928a29eadfb4d

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '92529fbe8fc5bf678405');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e2cf0eaf51c21aa2fcbb');
