1e1577c89ad615ccc291acf3ea66f430ff21f945
nateweller <nateweller@users.noreply.github.com>
2024-04-25 21:30:27 +0000

    A4A Client: Unify disconnect dialog styles (#37040)
    * Unify disconnect dialog styles
    
    * changelog
    
    * Fix border radius
    
    * Restore styles overwritten by merge
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8839509208
    
    Upstream-Ref: Automattic/jetpack@4fc507e5b01b9b21f6dfd6cede8b6868f289ddfd

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '00019c70ded3cb198668');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2ae06d7721393483fa75');
