70db6e6d5f17965b78abb5228dfd3e32d335a7de
ouikhuan <ouikhuan@users.noreply.github.com>
2024-05-16 11:37:28 +0000

    Changelog and readme.txt edits. (#37417)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9111393017
    
    Upstream-Ref: Automattic/jetpack@f4c384429cdf8a0d63d276631cecd86ee3be42dc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.10-alpha1715799056',
+      'ver' => '2.1.10',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.8.2-alpha1715847720',
+      'ver' => '2.8.2',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.19.0-alpha1715799056',
+      'ver' => '0.19.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.16.3-alpha1715793138',
+      'ver' => '2.16.3',
     ),
   ),
 );
