30146c60d53ce36a208cb1b41f11afeb301ca14d
tbradsha <tbradsha@users.noreply.github.com>
2024-05-20 19:22:01 +0000

    Backport mu-wpcom-plugin 2.1.27, jetpack 13.5-a.3 Changes (#37456)
    * Changelog and readme.txt edits.
    
    * Small tweaks
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9163719203
    
    Upstream-Ref: Automattic/jetpack@78d40351ea956cf913ab079e80b460798dc331d4

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.11-alpha1715875343',
+      'ver' => '2.1.11',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.8.3-alpha1715983705',
+      'ver' => '2.8.3',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.20.0-alpha1715868434',
+      'ver' => '0.20.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.16.4-alpha1716208720',
+      'ver' => '2.16.4',
     ),
   ),
 );
