8119c30167d68ad3c5f6e31ac5a1f795d540a953
tbradsha <tbradsha@users.noreply.github.com>
2024-06-03 15:28:43 +0000

    Backport mu-wpcom-plugin 2.1.29, jetpack 13.5-beta Changes (#37684)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9352791202
    
    Upstream-Ref: Automattic/jetpack@35527a973b55fc2e7a16fcc091396579d8ba7acb

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,11 +14,11 @@ return array(
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '2.0.3-alpha1717139684',
+      'ver' => '2.0.3',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.9.1-alpha1717417982',
+      'ver' => '2.9.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
