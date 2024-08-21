b6e538965f0907101545cd0528654417f3da529f
anomiex <anomiex@users.noreply.github.com>
2024-08-21 21:04:27 +0000

    Update dependency yoast/phpunit-polyfills to ^1.1.1 (#39004)
    * Update dependency yoast/phpunit-polyfills to 1.1.0 || 1.1.1
    * Caret version
    * wpcomsh: Remove unnecessary phpunit dep.
      `yoast/phpunit-polyfills` already brings in an appropriate version of
      phpunit.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10497266284
    
    Upstream-Ref: Automattic/jetpack@480d5da856bf545238871951e102ca8a200b5495

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.4.2',
+      'ver' => '0.4.3-alpha1724273335',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.3',
+      'ver' => '2.3.4-alpha1724273335',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,23 +18,23 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.12.3',
+      'ver' => '2.12.4-alpha1724273335',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.2.2',
+      'ver' => '0.2.3-alpha1724273335',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.1',
+      'ver' => '0.3.2-alpha1724273335',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.2.1',
+      'ver' => '0.2.2-alpha1724273335',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.7.0',
+      'ver' => '3.7.1-alpha1724273335',
     ),
   ),
 );
