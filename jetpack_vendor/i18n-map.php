96e2e7ad6a66032b28e829f70834eac5bdacdfd1
anomiex <anomiex@users.noreply.github.com>
2024-08-21 18:21:13 +0000

    Backport mu-wpcom-plugin 2.5.8, jetpack 13.8-a.5, wpcomsh 5.5.0 Changes (#38993)
    * Changelog and readme.txt edits.
    * Version bumps
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10495096273
    
    Upstream-Ref: Automattic/jetpack@828a8f2e8599e0f23e4ebb9287aa1293939ea9a0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.3-alpha1724171952',
+      'ver' => '2.3.3',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.12.3-alpha1724252288',
+      'ver' => '2.12.3',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.7.0-alpha1724141518',
+      'ver' => '3.7.0',
     ),
   ),
 );
