8e902a7af287be3b778755587b4e92931378cb44
allilevine <allilevine@users.noreply.github.com>
2024-08-23 15:44:18 +0000

    Changelog and readme.txt edits. (#39054)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10528329915
    
    Upstream-Ref: Automattic/jetpack@474f7f3bc62c0c86517546e2f1e06b6f414dac12

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.4.3-alpha1724273335',
+      'ver' => '0.4.3',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.4-alpha1724273335',
+      'ver' => '2.3.4',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,15 +18,15 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.12.4-alpha1724324484',
+      'ver' => '2.12.4',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.2.3-alpha1724273335',
+      'ver' => '0.2.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.2-alpha1724273335',
+      'ver' => '0.3.2',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.7.1-alpha1724273335',
+      'ver' => '3.7.1',
     ),
   ),
 );
