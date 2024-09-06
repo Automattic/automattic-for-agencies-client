fd511a9a58966c130778ceebf95ea4bc6539d5b5
anomiex <anomiex@users.noreply.github.com>
2024-09-06 20:58:57 +0000

    Revert "Disable terser conditional optimization (#39279)" (#39280)
    This reverts commit a4050bffedc64836cd9a41dfb7b700795bdd6a2b.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10744981873
    
    Upstream-Ref: Automattic/jetpack@100f517d0f912b7a825e2e6cfad2bd835088b7d5

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '58a4d83cf42ea9e387f6');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '732bbca0f06fe4991a49');
