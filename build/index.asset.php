e5ccf7289a36556683981d0906b38d419c718a86
zinigor <zinigor@users.noreply.github.com>
2024-09-09 17:32:34 +0000

    Lock file maintenance (#39288)
    * Lock file maintenance
    
    * Work around parsel-js assuming "browser" is esm
    
    * Downgrade framer-motion to 11.4.0 to avoid a deprecation
    
    Gutenberg is planning on removing the dep entirely, so let's not go to too much trouble to work around it.
    
    * Remove pnpmfile hack for fixed issue
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10778041548
    
    Upstream-Ref: Automattic/jetpack@1e3851d012f089018dc4da3b2709183324af732e

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '732bbca0f06fe4991a49');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '72beceed8ecee467043f');
