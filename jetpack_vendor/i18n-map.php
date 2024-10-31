662dfc65e03778e01345c0f9a64d1ede93c27a92
tbradsha <tbradsha@users.noreply.github.com>
2024-10-31 11:06:37 +0000

    Monorepo: Restore and add test coverage config (#39961)
    * Enable and normalize test coverage commands
    
    * Revert "Code Coverage: remove automation (#28542)"
    
    This reverts commit da5e78803f56ea0fe54f29720c05be037093b97a.
    
    * Revert "Remove remnants of automated code coverage support (#28982)"
    
    This reverts commit 8cc32461b2d3b7e3a4a391c033163e5ebc3822fa.
    
    * Fix starter plugin test broken by #31417
    
    * Plugin tests are now run in the monorepo rather than wp-content/plugins.
    
    * Removed undocumented WP_DEVELOP_DIR variable, documented WORDPRESS_DEVELOP_DIR and WORDPRESS_DIR
    
    * Fixed JS tests in some packages where .scripts.test-js had never been added to composer.json.
    
    ---------
    
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11610578911
    
    Upstream-Ref: Automattic/jetpack@7fadf4ab370a7ddf9605625b9d9089af4af3e300

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.4.5',
+      'ver' => '0.4.6-alpha1730371893',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.12',
+      'ver' => '2.3.13-alpha1730371893',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,23 +18,23 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '5.1.5',
+      'ver' => '5.1.6-alpha1730371893',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.3.0',
+      'ver' => '0.3.1-alpha1730371893',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.2',
+      'ver' => '0.3.3-alpha1730371893',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.2.3',
+      'ver' => '0.2.4-alpha1730371893',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.14.3',
+      'ver' => '3.14.4-alpha1730371893',
     ),
   ),
 );
