f81265defbf5912925c8d0ac161c3273cb9c72be
tbradsha <tbradsha@users.noreply.github.com>
2024-11-13 17:45:14 +0000

    General: Bump minimum PHP version to 7.2 (#40147)
    * Bump minimum WP version to 6.6
    
    * Match WP tested version to readme
    
    * Add changelogs
    
    * Remove 7.0 tests
    
    * Update comments with more recent data
    
    * Update composer.lock
    
    * Fix comment
    
    * Backup: Add next daily backup schedule time (#39914)
    
    * Set up react-query, moment and typescript
    
    * changelog
    
    * Add endpoint to fetch site backup schedule time
    
    * Setup react-query (queryClientProvider)
    
    * Add useScheduledTimeQuery hook to fetch schedule time
    
    * Add hook to parse and return the next backup schedule time
    
    * Add component to render the next scheduled backup message
    
    * Fix record Tracks event
    
    * Removing changelog for migration and core plugin
    
    * Bump `Automattic\Jetpack\Backup` namespace to V0005
    
    * changelog
    
    * Bump `Automattic\Jetpack\Transport_Helper` to V0005
    
    * Remove `between` to make `Modify` fit in the same line
    
    * Fix pnpm lock
    
    * Bump PHP 7.0 refs
    
    * Clean up false positive
    
    * setMethods → onlyMethods
    
    * Add changelogs
    
    * Bump PHP 7.0 refs
    
    * Clean up false positive
    
    * setMethods → onlyMethods
    
    * Add changelogs
    
    * More changelogs
    
    * Remove suppression for old PHP versions
    
    * Make PHPCS and Phan rules match
    
    * Docs tweak
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Remove comment
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Remove inaccurate changelogs
    
    * Make params explicitly nullable
    
    * Oops: onlyMethods → addMethods
    
    * Another onlyMethods → addMethods
    
    * Last one
    
    * Split existing and new methods to mock
    
    * Prevent error when not adding new methods
    
    * Update Phan baselines
    
    * Use a more recent theme
    
    * Remove unneeded @requires
    
    * Update composer.lock
    
    * Debug
    
    * More debug
    
    * Remove debug
    
    * Remove more debug
    
    * Oops, missed one
    
    * Add another changelog
    
    ---------
    
    Co-authored-by: Rafael Agostini <rafael.agostini@automattic.com>
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11822562293
    
    Upstream-Ref: Automattic/jetpack@e2ab2c1e50731acb008c3af2756b14568414fcc3

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,35 +6,35 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.4.6',
+      'ver' => '0.5.0-alpha1731518828',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.14',
+      'ver' => '3.0.0-alpha1731518828',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '2.0.4',
+      'ver' => '3.0.0-alpha1731518828',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '5.1.7',
+      'ver' => '6.0.0-alpha1731518828',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.3.1',
+      'ver' => '0.4.0-alpha1731518828',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.3.3',
+      'ver' => '0.4.0-alpha1731518828',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.2.4',
+      'ver' => '0.3.0-alpha1731518828',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.15.1-alpha1731512286',
+      'ver' => '4.0.0-alpha1731518828',
     ),
   ),
 );
