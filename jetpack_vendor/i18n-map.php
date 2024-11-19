f24e7bd0b06ea1df32bf927f1bfc011765d21a01
anomiex <anomiex@users.noreply.github.com>
2024-11-19 20:10:33 +0000

    Update dependency brain/monkey to ^2.6.2 (#40258)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11920722165
    
    Upstream-Ref: Automattic/jetpack@42ddde50812fd8c7a4427dcdc6f8c3dca7600d3c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.0-alpha1731948564',
+      'ver' => '4.0.0-alpha1732046180',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,11 +18,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.1.0-alpha1732030233',
+      'ver' => '6.1.0-alpha1732046180',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.0',
+      'ver' => '0.4.1-alpha1732046180',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
