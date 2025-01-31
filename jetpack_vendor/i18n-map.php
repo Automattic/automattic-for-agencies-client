87227b3a99b2223db3cc65cdbb1e68eac0fe39a7
kraftbj <kraftbj@users.noreply.github.com>
2025-01-31 15:34:37 +0000

    Dev: Only one WordPress (#41057)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13075594008
    
    Upstream-Ref: Automattic/jetpack@4597864460ce598b9d3ae6b168d596fd1f20305b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.2-alpha1738230497',
+      'ver' => '0.5.2-alpha1738336718',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.3.2-alpha1737997918',
+      'ver' => '6.3.2-alpha1738336718',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.1',
+      'ver' => '0.4.2-alpha1738336718',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.6.0-alpha1738231842',
+      'ver' => '4.6.0-alpha1738336718',
     ),
   ),
 );
