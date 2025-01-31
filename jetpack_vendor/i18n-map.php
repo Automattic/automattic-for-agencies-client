127aff08c94f2c00f357b612a02cafccb163186f
tbradsha <tbradsha@users.noreply.github.com>
2025-01-31 19:15:46 +0000

    Phan: Fix errant extra params in function calls (#41263)
    * Remove extra param on remove_filter/remove_action
    
    There's no arg count when removing.
    
    * Remove extra param from rest_ensure_response
    
    That function doesn't allow a status code to be passed. Presumably we could override the WP_REST_Response status and force 200, but passing it through has worked so far, so keeping it that way.
    
    * Remove other stray args
    
    In most cases, these appear to be due to refactors.
    
    * Another stray arg
    
    * Suppress things that confuse Phan
    
    * Fix get_avatar_url calls
    
    * arrayHasKey → assertArrayHasKey
    
    * Fix misplaced parentheses
    
    * Tighten types
    
    We're using `WP_Option`, which allows an extra param in its `get()`.
    
    * Add missing sprintf
    
    * Add unused param to allow dynamic method call
    
    * Add changelogs
    
    * Use same behaviour as trunk
    
    * Fix assertion
    
    * Add param to Storage::get and use that type as before
    
    * Inform Phan of type rather than suppressing error
    
    * Update errant types
    
    * Update Phan baselines
    
    * More changelogs
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13079055733
    
    Upstream-Ref: Automattic/jetpack@4b2a4a98c89a40054006e7f696be1ec7f516e852

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.3.2-alpha1738336718',
+      'ver' => '6.3.2-alpha1738349988',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.6.0-alpha1738336718',
+      'ver' => '4.6.0-alpha1738349988',
     ),
   ),
 );
