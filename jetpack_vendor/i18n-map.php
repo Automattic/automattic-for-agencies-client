6e5e2eda0341cc141600484fc8ad24c619b604f4
zinigor <zinigor@users.noreply.github.com>
2025-03-03 18:44:05 +0000

    Backport jetpack 14.4-beta Changes (#42188)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13637674210
    
    Upstream-Ref: Automattic/jetpack@aa012abb17c9dcef94ca4329be92e0dcda66b352

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.9-alpha1741025873',
+      'ver' => '4.0.10-alpha1741026079',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.0-alpha1741025873',
+      'ver' => '6.7.1-alpha1741026079',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
