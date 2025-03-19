deacb188722eb9ff67bf93e297ec44511a04d0ee
anomiex <anomiex@users.noreply.github.com>
2025-03-19 21:02:11 +0000

    Update PHPUnit stubs to PHPUnit 11.5 (#42591)
    This needs some changes to the munger:
    
    * `@psalm-` annotations seem mostly replaced with `@phpstan-`.
    * Some bad phpdocs have been fixed.
    
    This also calls for some cleanups of things deprecated or removed in
    PHPUnit 11:
    
    * `MockBuilder->addMethods()` is deprecated with no replacement. We
      often used this to "mock" stdClass; now, instead, we need to either
      use anonymous classes or mock a dummy interface.
    * `->getMockForAbstractClass()` is also deprecated. Can be replaced with
      `->onlyMethods( ... )->getMock()` but we have to manually list the
      needed methods.
    * `->getMockForTrait()` as well. Anonymous classes work well here.
    * `TestCase->returnValue()` is deprecated in favor of
      `$double->willReturn()`.
    * `TestCase->addWarning()` is gone. Seems to work replacing it with
      `trigger_error( E_USER_WARNING )`.
    * packages/masterbar: `Base_Admin_Menu_Test` has some of the same
      weirdness `WPcom_Admin_Menu_Test` did before #42504, just less
      obviously so.
    * plugins/jetpack: `Jetpack_Client_Server_Test` seems to have been
      creating unused mocks since #14043. Removed them.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13956378052
    
    Upstream-Ref: Automattic/jetpack@281a100a6e89c57e332b8b4584d8ff7f346e3bd5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.14-alpha1742415897',
+      'ver' => '4.0.14-alpha1742417137',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.7.7-alpha1742415897',
+      'ver' => '6.7.7-alpha1742417137',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
