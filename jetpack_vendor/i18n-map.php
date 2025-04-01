813897e6e9e5d4030859733a47c182f56c519296
donnchawp <donnchawp@users.noreply.github.com>
2025-04-01 15:28:22 +0000

    Changelog and readme.txt edits. (#42820)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14199309077
    
    Upstream-Ref: Automattic/jetpack@18955b77c8be486471d7965f08b704792f93aa0d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.15',
+      'ver' => '4.0.16',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.10.0',
+      'ver' => '6.10.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.7-alpha1743509206',
+      'ver' => '0.3.7',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
