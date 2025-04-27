e67263530a2ddd166452ddf643749e7d1d8336c4
kraftbj <kraftbj@users.noreply.github.com>
2025-04-27 21:48:02 +0000

    A4A: Resolve blank page caused by incorrectly parsing an error object. (#43221)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14696323239
    
    Upstream-Ref: Automattic/jetpack@5a3985c5ab147887d1f26853f879643a9b3315c1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f6850b05fdaf584a3f16');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b969cc73cff2a51491ef');
