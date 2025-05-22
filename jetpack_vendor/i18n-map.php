274fe43727030f6d5d8201dbeb6925393be36524
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-05-22 11:35:18 +0000

    Changelog and readme.txt edits. (#43569)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15185166901
    
    Upstream-Ref: Automattic/jetpack@e31eb7af3c212e6702d4bdcd5d6c6094c6238e3c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.20',
+      'ver' => '4.0.21',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.5',
+      'ver' => '6.11.6',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.10',
+      'ver' => '0.3.11-alpha1747912656',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
