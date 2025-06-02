88906e3d17cfdbdce7aa4794c4230aff5d1d1e7c
tbradsha <tbradsha@users.noreply.github.com>
2025-06-02 17:58:31 +0000

    Backport jetpack 14.7-beta Changes (#43732)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Clean up earlier release changelog for packages/forms
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15399027622
    
    Upstream-Ref: Automattic/jetpack@1163d44013be4d037f6d09647a255fce7da42bea

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.22',
+      'ver' => '4.0.23',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.7',
+      'ver' => '6.11.8',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.14.0-alpha1748353161',
+      'ver' => '4.14.0',
     ),
   ),
 );
