5d8ac078edec4e1616ae5a19959a17811acb7bed
tbradsha <tbradsha@users.noreply.github.com>
2025-06-19 13:50:02 +0000

    Backport WordAds final release (#44023)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15759183126
    
    Upstream-Ref: Automattic/jetpack@0bba48b87fef666ea6ce53178a8635b017bfecd7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,11 +14,11 @@ return array(
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '3.1.1-alpha1750336055',
+      'ver' => '3.1.1',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.5',
+      'ver' => '6.13.6',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
