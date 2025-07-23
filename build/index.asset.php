89580a30e4d779bd17eb6ac129ef905068c8bf65
tbradsha <tbradsha@users.noreply.github.com>
2025-07-23 13:50:58 +0000

    Stylelint: Address remaining stylistic rules (#44403)
    * Fix @stylistic/declaration-block-trailing-semicolon
    
    * Fix @stylistic/no-extra-semicolons
    
    * Fix @stylistic/no-eol-whitespace
    
    * Fix @stylistic/number-leading-zero and @stylistic/number-no-trailing-zeros
    
    * Fix @stylistic/color-hex-case
    
    * Fix @stylistic/property-case
    
    * Fix @stylistic/string-quotes
    
    * Fix @stylistic/max-line-length
    
    * Add changelogs
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16472168446
    
    Upstream-Ref: Automattic/jetpack@337c2ec36fff6fe4e3df7a66e9a592f79343cf54

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '33b5cb3c3656791b3f96');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a75253547631b1d85c1c');
