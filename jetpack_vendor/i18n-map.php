efd29ca302cb05c02a63128f647c2b3548f9b616
kraftbj <kraftbj@users.noreply.github.com>
2025-07-23 17:01:17 +0000

    Changelog and readme.txt edits. (#44432)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16476665154
    
    Upstream-Ref: Automattic/jetpack@6d6ccdc0b755408a7c2bb8900efe7ee152fb53fb

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.0-alpha1753277640',
+      'ver' => '4.3.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.16.0-alpha1753277640',
+      'ver' => '6.16.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.14-alpha1753277640',
+      'ver' => '0.3.14',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
