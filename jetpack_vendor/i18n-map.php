8fbfdcdecf3a59e23ce86e36c8a1baf18a3c5e9e
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-08-18 16:29:47 +0000

    Backport jetpack 15.0-a.5 Changes (#44823)
    * Changelog and readme.txt edits.
    
    * Apply suggestions from code review
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17046107894
    
    Upstream-Ref: Automattic/jetpack@77faa9d87336166a3268ef58b4d8793a09462cd3

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.4',
+      'ver' => '4.3.5',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.1-alpha1755189349',
+      'ver' => '6.18.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.19.0-alpha1755226945',
+      'ver' => '4.19.0',
     ),
   ),
 );
