76c83ab62ef25872ee3010225915b60a04e214f2
chihsuan <chihsuan@users.noreply.github.com>
2025-08-25 06:33:56 +0000

    Changelog and readme.txt edits (#44934)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17200796799
    
    Upstream-Ref: Automattic/jetpack@6ae5ef0fa69680d9be6008ab45c9850023f36a88

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.3-alpha1755886132',
+      'ver' => '6.18.3',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.20.0-alpha1755841039',
+      'ver' => '4.20.0',
     ),
   ),
 );
