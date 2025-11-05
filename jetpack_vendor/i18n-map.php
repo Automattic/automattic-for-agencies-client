ba492b9e5b2dd5e9bb1f1c05203b95197ebbad8e
tbradsha <tbradsha@users.noreply.github.com>
2025-11-05 13:46:16 +0000

    PHP 8.5: Wrap deprecated setAccessible method in PHP version check (#45769)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19103585802
    
    Upstream-Ref: Automattic/jetpack@920bbbd772c047f3ba8ac4169ea231f33f5072fe

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.12-alpha1762198372',
+      'ver' => '4.3.12-alpha1762349303',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.3',
+      'ver' => '6.19.4-alpha1762349303',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.22.3',
+      'ver' => '4.22.4-alpha1762349303',
     ),
   ),
 );
