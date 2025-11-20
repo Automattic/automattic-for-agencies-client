b99b1a552431dca65aeb01979929545209597982
tbradsha <tbradsha@users.noreply.github.com>
2025-11-20 21:41:59 +0000

    Updated package versions for automattic/automattic-for-agencies-client
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19552179880
    
    Upstream-Ref: Automattic/jetpack@e38167fb6396457f688050aea3503922cc7a9196

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.8-alpha1763651300',
+      'ver' => '6.19.8',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.20-alpha1763427690',
+      'ver' => '0.3.20',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.22.5-alpha1763654150',
+      'ver' => '4.22.5',
     ),
   ),
 );
