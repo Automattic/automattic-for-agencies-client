3dab95af836d0ce418f68697b11f8599ac486364
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-12-17 09:45:34 +0000

    Backport jetpack 15.4-a.3 changes (#46303)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Update projects/js-packages/charts/CHANGELOG.md
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20298026265
    
    Upstream-Ref: Automattic/jetpack@5988af7f799aab67b500fd596b2140cb598ab7cc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.17-alpha1765479562',
+      'ver' => '4.3.17',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.12-alpha1765479562',
+      'ver' => '6.19.12',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.24.1-alpha1765886304',
+      'ver' => '4.24.2-alpha1765963715',
     ),
   ),
 );
