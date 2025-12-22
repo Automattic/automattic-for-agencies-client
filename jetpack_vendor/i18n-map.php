314189195e93ae745c651a03fd99856e3e9715de
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-12-22 10:53:12 +0000

    Backport jetpack 15.4-a.5 changes (#46383)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Fix typo
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20429384995
    
    Upstream-Ref: Automattic/jetpack@d30c7fd4782e236aa8f4624b6c892b4bcefc14e0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.18-alpha1766165482',
+      'ver' => '4.3.18',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.13-alpha1766380116',
+      'ver' => '6.19.13',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.24.2-alpha1765963715',
+      'ver' => '4.24.2',
     ),
   ),
 );
