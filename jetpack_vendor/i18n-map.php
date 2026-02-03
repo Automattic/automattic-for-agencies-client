ee087fc8a19d8b8b762d2f172ab88458ebfd2776
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-02-03 06:19:49 +0000

    Backport jetpack 15.5-beta changes (#46893)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21618989026
    
    Upstream-Ref: Automattic/jetpack@ad261bfcf0cde8db954016ab0c1ab20db6612355

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.22-alpha1769795377',
+      'ver' => '4.3.22',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.0.0-alpha1769795377',
+      'ver' => '7.0.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.23',
+      'ver' => '0.3.24-alpha1770098352',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.26.0-alpha1770031438',
+      'ver' => '4.26.0',
     ),
   ),
 );
