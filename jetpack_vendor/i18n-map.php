44646dacff2f80fd7d33be86f57f82f46b1167cf
chihsuan <chihsuan@users.noreply.github.com>
2026-03-16 06:16:25 +0000

    Prepare woocommerce-analytics 0.16.0 release (#47600)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23130052013
    
    Upstream-Ref: Automattic/jetpack@cb820dccc6865f39726a50799773ecff980af34c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.28-alpha1773069364',
+      'ver' => '4.3.28',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.4-alpha1773069364',
+      'ver' => '8.0.4',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.25',
+      'ver' => '0.3.26-alpha1773640606',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
