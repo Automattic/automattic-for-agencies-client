50fd78d1f3db6a90a2d39a283f9fe5670a12560f
tbradsha <tbradsha@users.noreply.github.com>
2026-03-30 17:55:33 +0000

    Backport jetpack 15.7-a.7 changes (#47845)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23758668413
    
    Upstream-Ref: Automattic/jetpack@0884ade6c4bcc2113d0a70d000bd07e34dc8e863

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.6.0-alpha1774866039',
+      'ver' => '0.6.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.30-alpha1774534379',
+      'ver' => '4.3.30',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.1.0-alpha1774627570',
+      'ver' => '8.1.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.34.0-alpha1774519451',
+      'ver' => '4.34.0',
     ),
   ),
 );
