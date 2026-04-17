6098fbe6c79242b9bc6c07fec55487a09f178a7e
tbradsha <tbradsha@users.noreply.github.com>
2026-04-17 13:01:22 +0000

    Update wordpress monorepo (#48106)
    * Fix `@wordpress/ui` peer dep issue.
    * Charts: Rename WPDS font tokens to typography-* for @wordpress/theme@0.11.0
    * Fix TS errors
    * Clean up global types mess in mu-wpcom
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24565711396
    
    Upstream-Ref: Automattic/jetpack@5cca8ab3cec81465ff7fcc8d169be4983f83d7c6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.0',
+      'ver' => '0.8.1-alpha1776429876',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.32',
+      'ver' => '4.3.33-alpha1776429876',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.0',
+      'ver' => '8.3.1-alpha1776429876',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
