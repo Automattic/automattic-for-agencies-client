e5e45813093c1e603e3c7e3980d60bbd157af332
tbradsha <tbradsha@users.noreply.github.com>
2026-04-20 17:36:29 +0000

    Backport jetpack 15.8-a.5 changes (#48210)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24680426613
    
    Upstream-Ref: Automattic/jetpack@0da10a253407ac6898431fcc1357ac8adcecf6df

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.1-alpha1776429876',
+      'ver' => '0.8.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.33-alpha1776429876',
+      'ver' => '4.3.33',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.1-alpha1776429876',
+      'ver' => '8.3.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.26',
+      'ver' => '0.3.27-alpha1776705712',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
