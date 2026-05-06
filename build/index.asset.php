a63177b4ed8e1f9a8498e714c41de475495837ef
arcangelini <arcangelini@users.noreply.github.com>
2026-05-06 21:59:36 +0000

    Podcast: page chrome + empty tabs in dashboard route (3/4) (#48559)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25462771479
    
    Upstream-Ref: Automattic/jetpack@8d6cb61982145d1193e2dd94fdaed114ef0fc406

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '69a7cacf29614ec05c43');
+<?php return array('dependencies' => array('jetpack-connection', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4501d30e81c055fad93e');
