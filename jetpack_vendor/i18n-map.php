dc16d511c07bab68df6cf3fc5db663a5e3bc241a
tbradsha <tbradsha@users.noreply.github.com>
2026-05-19 08:38:37 +0000

    Phan: Address PhanPluginDuplicateConditionalNullCoalescing violations (#48887)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26085253845
    
    Upstream-Ref: Automattic/jetpack@2a848e21f72597df8312885f7f66e25849942e00

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.6-alpha1778793320',
+      'ver' => '8.3.6-alpha1779178862',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.38.2-alpha1778793320',
+      'ver' => '4.38.2-alpha1779178862',
     ),
   ),
 );
