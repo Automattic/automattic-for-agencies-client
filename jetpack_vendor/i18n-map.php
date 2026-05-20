39e570e9944047b4fc4ad8f2132f9656fdf9379b
anomiex <anomiex@users.noreply.github.com>
2026-05-20 19:07:19 +0000

    Update wordpress monorepo (#48405)
    * Update wordpress monorepo
    * Fix copy-pasted styles in search to match WordPress/gutenberg/77717
    * search: Also remove styles removed in WordPress/gutenberg/59096
    * components: Simplify buttons in footer and update tests
    * Bump eslint-plugin-react-hooks version
    * Fix tests failing due to uuid v14 going esm-only
    * Fix wp-build-polyfills test to not incorrectly fail on the dev build
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Mikael Korpela <mikael.korpela@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26183087836
    
    Upstream-Ref: Automattic/jetpack@94ba7494ccb69fc1b462262e89528e32db6b2821

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.5',
+      'ver' => '0.8.6-alpha1779302993',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.38-alpha1779296686',
+      'ver' => '4.3.38-alpha1779302993',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.7-alpha1779215895',
+      'ver' => '8.3.7-alpha1779302993',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
