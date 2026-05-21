7f0a15866f28259d9800d0336141fa00ff523e50
kangzj <kangzj@users.noreply.github.com>
2026-05-21 01:52:26 +0000

    bump search package version to 7.0.0 (#49029)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26200202450
    
    Upstream-Ref: Automattic/jetpack@79b65ae5760351ba598768db702f46dedc5e6685

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.6-alpha1779302993',
+      'ver' => '0.8.6',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.38-alpha1779302993',
+      'ver' => '4.3.38',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.7-alpha1779302993',
+      'ver' => '8.3.7',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.38.3-alpha1779225922',
+      'ver' => '4.38.3',
     ),
   ),
 );
