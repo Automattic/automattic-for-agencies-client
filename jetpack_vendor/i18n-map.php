da2591a4b9b5b5741d0e52b3b43d9338f260cc66
tbradsha <tbradsha@users.noreply.github.com>
2026-06-02 20:10:10 +0000

    Tests: Split code coverage scripts into test-js-coverage and test-php-coverage (#48845)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26844277266
    
    Upstream-Ref: Automattic/jetpack@b25e59181090ffcd52db1200e882dbb26ebc543e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.8',
+      'ver' => '0.8.9-alpha1780429966',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.39',
+      'ver' => '4.3.40-alpha1780429966',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,23 +18,23 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.2',
+      'ver' => '8.5.3-alpha1780429966',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.12',
+      'ver' => '0.4.13-alpha1780429966',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.11',
+      'ver' => '0.4.12-alpha1780429966',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.27-alpha1778782620',
+      'ver' => '0.3.27-alpha1780429966',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.39.0',
+      'ver' => '4.39.1-alpha1780429966',
     ),
   ),
 );
