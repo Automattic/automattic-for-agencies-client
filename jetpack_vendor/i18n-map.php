4a9b6742a22f38b99ed6d9260554949c18b187d8
zaguiini <zaguiini@users.noreply.github.com>
2026-06-05 14:21:57 +0000

    Release packages: Agents Manager and My Jetpack (#49431)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27019650649
    
    Upstream-Ref: Automattic/jetpack@be34c3d1b6d0438503afbd6baefc5247084bd279

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.0-alpha1780580341',
+      'ver' => '0.9.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.4-alpha1780573739',
+      'ver' => '8.5.4',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.12-alpha1780429966',
+      'ver' => '0.4.12',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.39.1-alpha1780429966',
+      'ver' => '4.39.1',
     ),
   ),
 );
