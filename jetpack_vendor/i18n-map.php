f5306efa3843f72294628693aedf9bb1d0cb13bd
anomiex <anomiex@users.noreply.github.com>
2026-06-08 20:20:03 +0000

    Update wordpress monorepo (#49273)
    * Update wordpress monorepo
    * Hack around dependency issues
      * `@wordpress/components` unnecessary dep on `@emotion/native`.
      * `@automattic/launchpad` outdated peer dep on `@wordpress/element`.
    * Clean up some obsolete pnpmfile hacks
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27163618273
    
    Upstream-Ref: Automattic/jetpack@170b24cec5d58635fee801eba296057c8ec99f1e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.1',
+      'ver' => '0.9.2-alpha1780949022',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.41',
+      'ver' => '4.3.42-alpha1780949022',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.5',
+      'ver' => '8.5.6-alpha1780949022',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
