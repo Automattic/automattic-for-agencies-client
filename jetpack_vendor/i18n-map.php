cf2df7ac0cd9b36007016f68dc4d6787e201d016
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-06-08 09:17:58 +0000

    Backport boost 4.6.0-beta changes (#49450)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27126919399
    
    Upstream-Ref: Automattic/jetpack@fc06f55557f03c9c1b5107bb559f2ffaff017208

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.1-alpha1780671753',
+      'ver' => '0.9.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.41-alpha1780671753',
+      'ver' => '4.3.41',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,23 +18,23 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.5-alpha1780671753',
+      'ver' => '8.5.5',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.14-alpha1780671753',
+      'ver' => '0.4.14',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.13-alpha1780671753',
+      'ver' => '0.4.13',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.27-alpha1780671753',
+      'ver' => '0.3.27',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.40.0-alpha1780904969',
+      'ver' => '4.40.0',
     ),
   ),
 );
