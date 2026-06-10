de726e00d9b5c55a7e83625f6df5e2305b2396b9
robertsreberski <robertsreberski@users.noreply.github.com>
2026-06-10 08:13:39 +0000

    Backport Jetpack 15.9 stable release (#49488)
    * Update changelog (package/blocks)
    
    * Forms: input sanitization and capability hardening
    
    * Update changelog
    
    * Bump version
    
    * Update changelog
    
    * Squash changelog
    
    * Regenerate readme.txt
    
    * Update stable tag
    
    * Collapse changelog
    
    * Improve changelog and regenerate readme.txt
    
    * Improve changelog and regenerate readme.txt
    
    * Remove the redudant changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27261797793
    
    Upstream-Ref: Automattic/jetpack@88034448a5d38d4ce31b16069178aa4451ca96c6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.2-alpha1780949022',
+      'ver' => '0.9.2',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.42-alpha1781029404',
+      'ver' => '4.3.43-alpha1781078138',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.6.0-alpha1781005495',
+      'ver' => '8.6.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.27',
+      'ver' => '0.3.28-alpha1781078138',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.42.0-alpha1781011949',
+      'ver' => '4.42.0-alpha1781078138',
     ),
   ),
 );
