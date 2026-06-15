66aed3b4a6d4d6f6d9916d1f181c12e3dc77b62f
robertsreberski <robertsreberski@users.noreply.github.com>
2026-06-15 13:46:53 +0000

    Backport jetpack 16.0-a.1 changes (#49612)
    * Baseline changelogger run
    
    * Human refinement of changelog
    
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Apply #49612 review feedback: drop redundant package prefixes, generalize hyperlink wording, terser sync entry
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27549651615
    
    Upstream-Ref: Automattic/jetpack@5fa05c1789356e7b7264ca32d25b680e595439b7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.6.1-alpha1781111196',
+      'ver' => '8.6.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.14-alpha1781111196',
+      'ver' => '0.4.14',
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.43.0-alpha1781526297',
+      'ver' => '4.43.1-alpha1781530122',
     ),
   ),
 );
