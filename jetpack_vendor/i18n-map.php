099927ac23bd6b5c40b31ca28df3d7642379d921
anomiex <anomiex@users.noreply.github.com>
2026-06-17 16:06:21 +0000

    Update wordpress monorepo (#49691)
    * Update wordpress monorepo
    * wp-build-polyfills: Bump `@wordpress/admin-ui`
      `@wordpress/boot` requires a symbol from the newer version.
    * Remove obsolete pnpmfile hack
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27701519589
    
    Upstream-Ref: Automattic/jetpack@d9374873f8eb59915f8a874883bc2394ce46efc8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.3',
+      'ver' => '0.9.4-alpha1781711227',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.44',
+      'ver' => '4.3.45-alpha1781711227',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.1-alpha1781619594',
+      'ver' => '8.7.1-alpha1781711227',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
