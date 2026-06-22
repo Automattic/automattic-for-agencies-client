b48bcc23361cbf44a3dd972d2e09c6707d5b670a
anomiex <anomiex@users.noreply.github.com>
2026-06-22 19:32:06 +0000

    Update dependency webpack to v5.107.2 (#49831)
    * Update dependency webpack to v5.107.2
    * Update indirect `http-proxy-middleware` dep too
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27977562696
    
    Upstream-Ref: Automattic/jetpack@71faf046c9cc9ab9a7cfb02ef80d3a204ea81c64

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.4',
+      'ver' => '0.9.5-alpha1782155618',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.0',
+      'ver' => '4.4.1-alpha1782155618',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.2-alpha1782152687',
+      'ver' => '8.7.2-alpha1782155618',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-plugin-deactivation' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugin-deactivation',
-      'ver' => '0.3.29-alpha1781567893',
+      'ver' => '0.3.29-alpha1782155618',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
