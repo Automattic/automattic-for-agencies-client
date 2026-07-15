6375dbbd92a9ebc77e4699fdabbe8d2e94be9b37
enejb <enejb@users.noreply.github.com>
2026-07-15 20:51:40 +0000

    Changelog and readme.txt edits. (#50551)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29448865409
    
    Upstream-Ref: Automattic/jetpack@e3407cc849e8f6dfea5b12944c65a3fde934f667

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.9',
+      'ver' => '0.9.10',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.5',
+      'ver' => '4.4.6-alpha1784147716',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.8',
+      'ver' => '8.7.9-alpha1784147716',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
