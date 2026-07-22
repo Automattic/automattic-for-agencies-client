61688e60cd6ac8885da0588c36654ad5a781dbe1
kangzj <kangzj@users.noreply.github.com>
2026-07-22 05:15:38 +0000

    Backport search 7.0.1 changes (#50719)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29892460686
    
    Upstream-Ref: Automattic/jetpack@8defbfb3fb7b15fc2ea79b87d83d2ea5a565fcd6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.11',
+      'ver' => '0.9.12',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.6',
+      'ver' => '4.4.7',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.9',
+      'ver' => '8.7.10',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
