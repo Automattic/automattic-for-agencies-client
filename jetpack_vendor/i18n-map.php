164d055be24417af86e629ad2b732c6240ee5d03
alshakero <alshakero@users.noreply.github.com>
2026-07-31 09:35:07 +0000

    Connection: Prepare 8.8.1 release (#50958)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30618747253
    
    Upstream-Ref: Automattic/jetpack@18e8de866ca3cbc8930ae0f5fe9df1e5d26e4a6b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.13',
+      'ver' => '0.9.14',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.9-alpha1785220502',
+      'ver' => '4.4.9',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.8.1-alpha1785405980',
+      'ver' => '8.8.1',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
