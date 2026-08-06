6f0727fd81474c2ad20110e81152532a0929fcba
aaronfc <aaronfc@users.noreply.github.com>
2026-08-06 15:02:09 +0000

    Backport: Agents Manager 0.9.1 (#51095)
    * Agents Manager: stand-alone release 0.9.1
    
    * Apply changelog suggestions from code review
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31112234735
    
    Upstream-Ref: Automattic/jetpack@8534d74c40bd9efab65aee50ac98ffb1efcbb57e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.15-alpha1785922550',
+      'ver' => '0.9.15',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.10-alpha1785918469',
+      'ver' => '4.4.10',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.9.0-alpha1785998793',
+      'ver' => '8.9.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
