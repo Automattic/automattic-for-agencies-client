4dfc2e46ba89f3a9e1c53713742f15d47d73bc1a
coder-karen <coder-karen@users.noreply.github.com>
2026-08-12 09:28:27 +0000

    Connection: Report signing failures with correct codes and attribution (#51116)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31581671645
    
    Upstream-Ref: Automattic/jetpack@4d143748076b7801ab445f097c9875bb1f3080b2

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.10.0',
+      'ver' => '8.10.1-alpha1786525793',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -74,9 +74,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-connection' => array(
-            'pretty_version' => '8.10.0',
-            'version' => '8.10.0.0',
-            'reference' => 'a91da1019c21dd689d1aeb979b86e2ee4c29d76a',
+            'pretty_version' => '8.10.1-alpha.1786525793',
+            'version' => '8.10.1.0-alpha1786525793',
+            'reference' => 'f4039b5d25b81545c4444e22bcdc6129f35b91df',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-connection',
             'aliases' => array(),
@@ -148,7 +148,7 @@
         'automattic/jetpack-sync' => array(
             'pretty_version' => '4.45.3',
             'version' => '4.45.3.0',
-            'reference' => '140f5ca3cea571922e27f9f52d860ff545bdfa93',
+            'reference' => '287264c99770218b2f64ed91cc57831cb17d60d5',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-sync',
             'aliases' => array(),
