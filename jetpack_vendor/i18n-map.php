12bb46f0658f69d325635d3360ac6dcdcaaa0402
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-08-13 20:32:25 +0000

    Backport search 7.1.0 changes (#51263)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Adding missing changelog to prerelease PR.
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31739861839
    
    Upstream-Ref: Automattic/jetpack@7f711a4c950261d0770dd97064ee85d00b404688

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.10.3-alpha1786644681',
+      'ver' => '8.10.2',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -74,9 +74,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-connection' => array(
-            'pretty_version' => '8.10.3-alpha.1786644681',
-            'version' => '8.10.3.0-alpha1786644681',
-            'reference' => 'ee07f1a857cd2db1117c6e022bcb0bf708805ea7',
+            'pretty_version' => '8.10.2',
+            'version' => '8.10.2.0',
+            'reference' => '51d7c9849096dffcaa8b2cb43b54ecd07427a111',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-connection',
             'aliases' => array(),
@@ -148,7 +148,7 @@
         'automattic/jetpack-sync' => array(
             'pretty_version' => '4.45.3',
             'version' => '4.45.3.0',
-            'reference' => '456d92d12a7e6a0387718f95903ca97991e228f6',
+            'reference' => 'add2e7782f9ea7b097a70b16199e7da5f1552650',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-sync',
             'aliases' => array(),
