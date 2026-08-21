8957f3e1b9954319e8ece23e480bf7306ce0303d
coder-karen <coder-karen@users.noreply.github.com>
2026-08-21 09:09:22 +0000

    Connection: Update error handling docs (#51421)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32465128971
    
    Upstream-Ref: Automattic/jetpack@8dfe3cd1beceda5497c620c109f42d4ae57bd1b2

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.11.0',
+      'ver' => '8.11.1-alpha1787302300',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -74,9 +74,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-connection' => array(
-            'pretty_version' => '8.11.0',
-            'version' => '8.11.0.0',
-            'reference' => 'a0dede4e30d371537138a88512445aa169d7643c',
+            'pretty_version' => '8.11.1-alpha.1787302300',
+            'version' => '8.11.1.0-alpha1787302300',
+            'reference' => '45f17d64f664bf73f6c967e6ee8c1a7dbfa0aa58',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-connection',
             'aliases' => array(),
@@ -148,7 +148,7 @@
         'automattic/jetpack-sync' => array(
             'pretty_version' => '4.45.4',
             'version' => '4.45.4.0',
-            'reference' => 'f52a5628bc9d06e3f1c5ba312c751a373a49e1c8',
+            'reference' => 'c38cd8117453ce5bc705c63d152bf7ac4dfddb96',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-sync',
             'aliases' => array(),
