c242e45546115421d6cb00a3c23062b235f1e2d5
Nikschavan <Nikschavan@users.noreply.github.com>
2026-08-25 09:02:43 +0000

    Connection: enqueue the SSO login and users-table styles (#51460)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32828361053
    
    Upstream-Ref: Automattic/jetpack@a6b4a108db25a9e27c979fef1318b6c3c949e349

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -74,9 +74,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-connection' => array(
-            'pretty_version' => '8.11.1-alpha.1787549563',
-            'version' => '8.11.1.0-alpha1787549563',
-            'reference' => '21e628071748f5fb8f75a6ed40eeb8225171a97b',
+            'pretty_version' => '8.12.0-alpha.1787647579',
+            'version' => '8.12.0.0-alpha1787647579',
+            'reference' => 'd220837fb92a336350f7477182f5c44418405136',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-connection',
             'aliases' => array(),
@@ -148,7 +148,7 @@
         'automattic/jetpack-sync' => array(
             'pretty_version' => '4.45.4',
             'version' => '4.45.4.0',
-            'reference' => 'c38cd8117453ce5bc705c63d152bf7ac4dfddb96',
+            'reference' => 'c6cada5883c4f4ce648571bf6c6e15beff37e56e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-sync',
             'aliases' => array(),
