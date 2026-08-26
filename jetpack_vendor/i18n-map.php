425ebbe8c91578de1c2c15ac914b6687df1eb68c
anomiex <anomiex@users.noreply.github.com>
2026-08-26 19:46:01 +0000

    Fix various things directly mentioning PHP <7.4 (#51575)
    Mostly comments that needed updating. A few places had actual code
    changes:
    
    * Jetpack's `jetpack_shim_setcookie()` is no longer needed, can be
      deprecated.
    * Search had a bunch of places avoiding `assertMatchesRegularExpression`
      which didn't exist in PHPUnit 8.
    * Various places accounting for `proc_open()` not taking an array for
      `$command` until PHP 7.4.
    * A few tests being skipped in older PHP versions.
    
    There was one thing I saw but didn't know what to do with:
    
    * A bunch of agent files about the WordPress Abilities API. Since we
      dropped WP 6.9 support, the relevant code may need more cleanup or
      deprecation than just updating version numbers.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33004877931
    
    Upstream-Ref: Automattic/jetpack@c39c628208bc182fd92cdfce044eec679021f81a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.0.0-alpha1787723748',
+      'ver' => '9.0.0-alpha1787772195',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.0.0-alpha1787677819',
+      'ver' => '5.0.0-alpha1787772195',
     ),
   ),
   'paths' => array(
