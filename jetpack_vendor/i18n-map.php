bd220a17eb1e365b59b29c5c48c4f03602cc72c1
coder-karen <coder-karen@users.noreply.github.com>
2026-09-01 13:36:23 +0000

    Connection: Make connection error gating direction-aware (#51387)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33512612690
    
    Upstream-Ref: Automattic/jetpack@a66ccebfce47b57f3589c435c510a80cb3202a96

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.0-alpha1788158628',
+      'ver' => '9.1.0-alpha1788268718',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
