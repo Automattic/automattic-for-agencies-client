95865433e209ad637a4e679ecd1a0825ea4d790d
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-09-01 17:40:00 +0000

    Backport jetpack 16.2-a.5 changes (#51843)
    * Changelog and readme.txt edits.
    
    * Include Jetpack AI settings fix in 16.2-a.5
    
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33536961377
    
    Upstream-Ref: Automattic/jetpack@257852d4b1145bd2022ae7590b22dc22823ccc4e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.11.1-alpha1787829579',
+      'ver' => '0.11.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.0',
+      'ver' => '5.0.1',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.0-alpha1788269986',
+      'ver' => '9.1.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.0.1-alpha1787942239',
+      'ver' => '5.0.1',
     ),
   ),
   'paths' => array(
