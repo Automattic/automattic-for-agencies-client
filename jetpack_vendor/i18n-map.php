16f7eb9cbb7b43a84c7b914705d18fc7c922303a
anomiex <anomiex@users.noreply.github.com>
2026-09-02 15:55:35 +0000

    tests: Use PHPUnit `assertMatchesRegularExpression` when applicable (#51893)
    These could have been being used all along with
    `Yoast\PHPUnitPolyfills\Polyfills\AssertionRenames`, but it seems devs
    weren't aware of that and did like `assertSame( 1, preg_match( ... ) )`
    in various places.
    
    Now that we've dropped support for PHP <7.4 and therefore PHPUnit <9.6,
    we don't even need the polyfill anymore.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33649083559
    
    Upstream-Ref: Automattic/jetpack@a41fd337bc760aa7a44e478f5e764f28223331b7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.1-alpha1788363066',
+      'ver' => '9.1.1-alpha1788363074',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
