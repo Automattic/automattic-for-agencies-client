008e318a6aac8cc4c4a09b188594cf54319c84bb
anomiex <anomiex@users.noreply.github.com>
2026-09-02 16:08:34 +0000

    tests: Use PHPUnit `assertIsType` assertions (#51895)
    These tend to give better output on failure versus things like
    `assertTrue( is_string() )`.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33650896735
    
    Upstream-Ref: Automattic/jetpack@930f7c5f271292fc7aaebab409e90121b2d2bd59

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.1-alpha1788363074',
+      'ver' => '9.1.1-alpha1788364081',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
