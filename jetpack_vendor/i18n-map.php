8f6221829d106dfb58d4c787ee4dcf5c368845df
interlineae <interlineae@users.noreply.github.com>
2026-09-03 09:15:37 +0000

    Search: prevent AI Answers paywall bypasses (#51890)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33736073442
    
    Upstream-Ref: Automattic/jetpack@a32661145726aacbe594712300cbd00e87a31115

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.1.0-alpha1788387674',
+      'ver' => '5.1.0-alpha1788425772',
     ),
   ),
   'paths' => array(
