ad5f93d720fc6e31fe65a9ad87b6f28bc50fd014
coder-karen <coder-karen@users.noreply.github.com>
2026-09-09 14:36:44 +0000

    Connection: Do not show errors users can't resolve (#52049)
    Co-authored-by: Foteini Giannaropoulou <giannaropoulou.foteini@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34362673101
    
    Upstream-Ref: Automattic/jetpack@d93054e70804ff684ff87ad0150640390422b00b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.2-alpha1788943628',
+      'ver' => '9.1.2-alpha1788963466',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
