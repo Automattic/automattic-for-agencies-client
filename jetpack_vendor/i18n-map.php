5895c61abfe8b0b29e80f229671aa691b3024932
arcangelini <arcangelini@users.noreply.github.com>
2026-09-15 21:04:37 +0000

    Comments: add WordPress.com, Google and Facebook sign-in through a popup (#52166)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35021341277
    
    Upstream-Ref: Automattic/jetpack@bb069fe9c1e87778c3c1e55888aa0f88e907d329

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.1.1-alpha1789467601',
+      'ver' => '5.1.1-alpha1789504981',
     ),
   ),
   'paths' => array(
