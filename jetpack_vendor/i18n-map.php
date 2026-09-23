fbfa56a98587cfd4f679450ec2512b22b489fd3e
vianasw <vianasw@users.noreply.github.com>
2026-09-23 20:26:18 +0000

    Backport videopress 3.6 changes (#52738)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35913556373
    
    Upstream-Ref: Automattic/jetpack@5e81437397228763638ba11f9739571389789bf7

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.14.0-alpha1790176704',
+      'ver' => '0.14.0',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.7.0-alpha1790168163',
+      'ver' => '9.7.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.2.0-alpha1790176159',
+      'ver' => '5.2.0',
     ),
   ),
   'paths' => array(
